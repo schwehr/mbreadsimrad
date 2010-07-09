3b9192e716922af6fd85b0df06b6f7ef67f501b9
schwehr <schwehr@a19cddd1-5311-0410-bb07-9ca93daf0f0b>
2010-07-09 12:36:42 +0000

    now how a working iterator
    git-svn-id: https://cowfish.unh.edu/projects/schwehr/trunk/src/mbreadsimrad@14104 a19cddd1-5311-0410-bb07-9ca93daf0f0b

--- mbsimrad.py
+++ mbsimrad.py
@@ -6,6 +6,10 @@ from __future__ import print_function
 import mmap   # load the file into memory directly so it looks like a big array
 import os
 import struct
+import operator
+
+class SimradError(Exception):
+    pass
 
 datagram_type_lut = {
     'D': ('depth',),
@@ -29,10 +33,15 @@ class Simrad(object):
         self.size = os.path.getsize(filename)
         self.data = mmap.mmap(tmp_file.fileno(), self.size, access=mmap.ACCESS_READ)
 
+    def __iter__(self):
+        iter = SimradIterator(self)
+        return iter
+
 class Datagram(object):
     def __init__(self, data, offset):
         'Data is a memory block with a data gram that starts at offset bytes into the block'
         self.data = data
+        self.offset = offset
         self.length = struct.unpack('I',data[offset:offset+4])[0]
         print ('length:',self.length)
         print ('STX:',ord(data[offset+4]))
@@ -43,11 +52,58 @@ class Datagram(object):
         for i in range(self.length-5,self.length+5):
             print (self.length,i,':',data[offset+i],ord(data[offset+i]))
 
-        assert(3 == ord(data[offset+self.length+1]))
-        self.checksum = struct.unpack('H',data[offset+self.length+2:offset+self.length+4])[0]
-        print ('checksum:',self.checksum)
-        map(ord,data[
-        
-simrad = Simrad('0034_20100604_005123_Healy.all')
-dg = Datagram(simrad.data,0)
+        assert(3 == ord(data[offset+self.length+1])) # End marker
+        self.checksum_reported = struct.unpack('H',data[offset+self.length+2:offset+self.length+4])[0]
+        self.checksum_actual = sum(map(ord,data[offset+5:offset+self.length]))
+        print ('checksum:',self.checksum_actual,self.checksum_reported)
 
+    def __str__(self): return self.__unicode__()
+    def __unicode__(self):
+        return 'dg: {length} {dg_type}, {dg_name}'.format(
+            length = self.length,
+            dg_type = dg.dgram_type,
+            dg_name = datagram_type_lut[dg.dgram_type],
+            )
+
+    def checksum_valid(self):
+        return self.checksum_reported == self.checksum_actual
+
+    def next(self):
+        'return the offset value for the next datagram'
+        return self.offset + self.length + 4
+
+class SimradIterator(object):
+    def __init__(self,simrad):
+        self.data = simrad.data
+        self.offset = 0
+        self.size = simrad.size
+    def __iter__(self):
+        return self
+    def next(self):
+        if self.offset > self.size:
+            raise StopIteration
+        dg = Datagram(self.data,self.offset)
+        self.offset = dg.next()
+        return dg
+    
+# Not used... just me playing
+def SimradGenerator(simrad):
+    'Iterate across the datagrams'
+    data = simrad.data
+    offset = 0
+    size = simrad.size
+    while offset<simrad.size:
+        dg = Datagram(data,offset)
+        offset = dg.next()
+        yield dg
+
+
+
+if __name__ == '__main__':
+    simrad = Simrad('0034_20100604_005123_Healy.all')
+    #dg = Datagram(simrad.data,0)
+    #iter = simrad.__iter__()
+    #print 
+    for count,dg in enumerate(simrad):
+        #print (count, dg.length, dg.dgram_type, datagram_type_lut[dg.dgram_type])
+        print (count, str(dg))
